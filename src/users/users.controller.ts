39bbd3bf0f21bdf9c71fe2aa38b2a2b1b0a25a5b
pthieenlong <p.thieenlong.304@gmail.com>
2024-09-19 09:52:35 +0700

    add User and Report Module and Entities. Settings thoses two on Service and Controller.

---
+++ src/users/users.controller.ts
@@ -0,0 +1,11 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDTO } from './dtos/create-user.dto';
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDTO) {
+    console.log(body);
+    
+  }
+
+}
